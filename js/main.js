f8c6272e3a5e59118517b872139397c3e4483265
Hekk3 <timurh434@gmail.com>
2020-08-25 20:02:41 +0300

    Add remove Group

--- js/main.js
+++ js/main.js
@@ -3,17 +3,22 @@ const allGroups = document.querySelector('.side-bar-content'); // all groups in 
 
 
 let createGroup = (e) => {
+	// create elements
 	const group = document.createElement('div'); // create the a group
 	const groupBtn = document.createElement('div'); // create button inside group
+	const groupBtnDelete = document.createElement('div'); // create button for delete group
 
-
+	// add classes for elements
 	group.classList.add('group-style');
 	groupBtn.classList.add('group-style-btn');
+	groupBtnDelete.classList.add('group-style-btn-delete');
 
-
+	// add elements to anoter elements
 	allGroups.append(group);
 	group.prepend(groupBtn);
+	group.prepend(groupBtnDelete);
 
+	// add lists
 	groupBtn.addEventListener('click', () => {
 		const list = document.createElement('div');
 
@@ -22,6 +27,12 @@ let createGroup = (e) => {
 
 
 		group.append(list);
+	});
+	// delete group
+	groupBtnDelete.addEventListener('click', () => {
+		group.classList.add('group-delete-anim');
+
+		setTimeout(() => group.remove(), 300);
 	});
 };
 
