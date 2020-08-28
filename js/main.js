bf76523894e94353f394e0dd5a9d8d3609e54291
Hekk3 <timurh434@gmail.com>
2020-08-28 21:03:08 +0300

    Add enter the name of group

--- js/main.js
+++ js/main.js
@@ -4,18 +4,27 @@ const allGroups = document.querySelector('.side-bar-content'); // all groups in 
 
 let createGroup = (e) => {
 	// create elements
-	const group = document.createElement('div'); // create the a group
-	const groupBtn = document.createElement('div'); // create button inside group
-	const groupBtnDelete = document.createElement('div'); // create button for delete group
+	const group = document.createElement('div'); // create the group
+	const groupBtn = document.createElement('div'); // create button inside the group
+	const groupBtnDelete = document.createElement('div'); // create button for delete the group
+	const inputName = document.querySelector('.add-name-group').value;
+	const nameGroup = document.createElement('p');
 
 	// add classes for elements
 	group.classList.add('group-style');
 	groupBtn.classList.add('group-style-btn');
 	groupBtnDelete.classList.add('group-style-btn-delete');
+	nameGroup.classList.add('name-group');
+
+	// add name for group
+
+	inputName === "" ? nameGroup.textContent = 'group' : nameGroup.textContent = inputName ;
+
 
 	// add elements to anoter elements
 	allGroups.append(group);
 	group.prepend(groupBtn);
+	group.prepend(nameGroup);
 	group.prepend(groupBtnDelete);
 
 	// add lists
