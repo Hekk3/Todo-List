63762820548a2cedb5a955bb612068911a3292ec
Hekk3 <timurh434@gmail.com>
2020-08-29 19:53:10 +0300

    add hide lists

--- js/main.js
+++ js/main.js
@@ -9,15 +9,17 @@ let createGroup = (e) => {
 	const groupBtnDelete = document.createElement('div'); // create button for delete the group
 	const inputName = document.querySelector('.add-name-group').value;
 	const nameGroup = document.createElement('p');
+	const listHide = document.createElement('div');
 
 	// add classes for elements
 	group.classList.add('group-style');
 	groupBtn.classList.add('group-style-btn');
 	groupBtnDelete.classList.add('group-style-btn-delete');
 	nameGroup.classList.add('name-group');
+	listHide.classList.add('list-hide');
+
 
 	// add name for group
-
 	inputName === "" ? nameGroup.textContent = 'group' : nameGroup.textContent = inputName ;
 
 
@@ -26,17 +28,30 @@ let createGroup = (e) => {
 	group.prepend(groupBtn);
 	group.prepend(nameGroup);
 	group.prepend(groupBtnDelete);
+	group.prepend(listHide);
 
-	// add lists
-	groupBtn.addEventListener('click', () => {
+
+	const createList = () => {
 		const list = document.createElement('div');
-
 
 		list.classList.add('list-style');
 
-
 		group.append(list);
+
+		// hide list
+		listHide.addEventListener('click', () => {
+
+			list.classList.toggle('list-hide-active');
+		});
+	}
+
+
+	// add lists
+	groupBtn.addEventListener('click', () => {
+		createList();
 	});
+
+
 	// delete group
 	groupBtnDelete.addEventListener('click', () => {
 		group.classList.add('group-delete-anim');
