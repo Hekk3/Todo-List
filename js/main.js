bccd81536ab9f84f8daee11f8aeccb849a3f5921
Hekk3 <timurh434@gmail.com>
2020-09-12 19:38:37 +0300

    A opportunity add a tasks.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 const btnAddGroup = document.querySelector('.add-group');
-const allGroups = document.querySelector('.side-bar-content'); // all groups in "side-bar-content"
 
-
-let createGroup = (e) => {
+const createGroup = () => {
 	// create elements
 	const group = document.createElement('div'); // create the group
 	const addListBtn = document.createElement('div'); // create button inside the group
 	const groupBtnDelete = document.createElement('div'); // create button for delete the group
-	const inputName = document.querySelector('.add-name-group').value; // get value from input
+	const inputNameGroup = document.querySelector('.add-name-group').value; // get value from input
+	const allGroups = document.querySelector('.side-bar-content'); // all groups in "side-bar-content"
 	const nameGroup = document.createElement('p');
 	const listHide = document.createElement('div');
 
@@ -27,7 +26,7 @@ let createGroup = (e) => {
 
 
 	// add name for group
-	inputName === "" ? nameGroup.textContent = 'group' : nameGroup.textContent = inputName ;
+	inputNameGroup === "" ? nameGroup.textContent = 'group' : nameGroup.textContent = inputNameGroup ;
 
 
 	// add content for a button - addListBtn
@@ -42,34 +41,9 @@ let createGroup = (e) => {
 	group.prepend(listHide);
 
 
-	const addList = () => {
-		const list = document.createElement('div');
-
-
-		// this is a check. We check, if we have an elements, where is a class -- list-hide-active, we remove it;
-		for (let i = 0; i < group.childNodes.length; i++) {
-			group.childNodes[i].classList.remove('list-hide-active');
-		}
-
-
-		// add class for list
-		list.classList.add('list-style');
-
-
-		// add the list in the group
-		group.append(list);
-
-
-		// hide list
-		listHide.addEventListener('click', () => {
-			list.classList.toggle('list-hide-active');
-		});
-	}
-
-
 	// add lists
 	addListBtn.addEventListener('click', () => {
-		addList();
+		createList(group, listHide, groupBtnDelete);
 	});
 
 
@@ -79,6 +53,94 @@ let createGroup = (e) => {
 
 		setTimeout(() => group.remove(), 300);
 	});
+};
+
+const createList = (group, listHide, groupBtnDelete) => {
+	const list = document.createElement('div');
+	const inputNameList = document.createElement('input');
+	const btnListDelete = document.createElement('div');
+
+	// add name list on default.
+	inputNameList.value = "List";
+
+
+	// this is a check. We check, if we have an elements, where is a class -- list-hide-active, we remove it;
+	for (let i = 0; i < group.childNodes.length; i++) {
+		group.childNodes[i].classList.remove('list-hide-active');
+	}
+
+
+	// add class for list and etc
+	list.classList.add('list-style');
+	inputNameList.classList.add('input-list');
+	btnListDelete.classList.add('btn-list-delete');
+
+	// add atribute
+	inputNameList.setAttribute('maxlength', 20);
+
+
+	// add the list in the group
+	group.append(list);
+	// add inputName for list
+	list.append(inputNameList);
+	list.append(btnListDelete);
+
+
+	// hide list
+	listHide.addEventListener('click', () => {
+		list.classList.toggle('list-hide-active');
+	});
+
+
+	// Delete list
+	btnListDelete.addEventListener('click', () => {
+		list.classList.add('list-delete-anim');
+
+		setTimeout(() => list.remove(), 300);
+	});
+
+
+	// Create content in the list with task inside;
+	createListContent(list, btnListDelete, groupBtnDelete);
+};
+
+const createListContent = (list, btnListDelete, groupBtnDelete) => {
+	const	tasksBody = document.querySelector('.tasks'),
+			taskContainer = document.createElement('div'),
+			taskContainerPanel = document.createElement('div');
+
+
+	taskContainer.classList.add('task-container');
+	taskContainerPanel.classList.add('task-container__panel')
+
+
+	tasksBody.append(taskContainer);
+	taskContainer.append(taskContainerPanel);
+
+
+	// hide taskContainer
+	list.addEventListener('click', () => {
+		const allTasks = document.querySelectorAll('.task-container');
+
+
+		for (let i = 0; i < allTasks.length; i++) {
+			allTasks[i].classList.remove('task-container_active');
+		}
+
+
+		taskContainer.classList.add('task-container_active');
+	});
+
+
+	// remove taskContainer
+	btnListDelete.addEventListener('click', () => {
+		taskContainer.remove();
+	});
+
+	groupBtnDelete.addEventListener('click', () => {
+		taskContainer.remove();
+	});
+
 };
 
 
